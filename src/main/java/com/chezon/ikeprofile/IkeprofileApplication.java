d8689cf3fd02f718ba13f3a722e6d8add1ff15e5
Ike Igboebisi <ikechezona@gmail.com>
2024-08-10 11:16:00 -0400

    initial commit.

---
+++ src/main/java/com/chezon/ikeprofile/IkeprofileApplication.java
@@ -0,0 +1,13 @@
+package com.chezon.ikeprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkeprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkeprofileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chezon/ikeprofile/IkeprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chezon.ikeprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkeprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
